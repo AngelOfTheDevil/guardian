4132566cbd0d2d5a5bb9cde86b9790c10c0b5040
Evelyne Lachance <eslachance@gmail.com>
2017-01-13 08:58:46 -0500

    Invalidated bot token because... derp

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 const komada = require('komada');
-komada.start({
-  "botToken": "MjY2NjM1NTUxMDczODk0NDEw.C1AjOg.YtuEMIumWW379ylgc-diFAKyP-s",
-  "ownerID" : "139412744439988224",
-  "clientID": "266635551073894410",
-  "functions": [],
-  "disabledCommands": [],
-  "commandInhibitors": ["disable", "permissions", "missingBotPermissions", "commandSlowMode"],
-  "databaseModules": [],
-  "clientOptions": { "fetchAllMembers": true }
-});
+const config = require('./config.json');
+komada.start(config);
